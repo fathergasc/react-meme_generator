467430b95b40343ea2ed800a140d10469c9180ab
fathergasc <quaglioniandrea@gmail.com>
2022-12-03 15:22:33 +0100

    add: header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import trollface from '../img/troll-face.png'
+
 export default function Header() {
     return (
-        <div>
-            Header
-        </div>
+        <header>
+            <img src={trollface} alt="" />
+            <span>Meme Generator</span>
+        </header>
     )
 }
